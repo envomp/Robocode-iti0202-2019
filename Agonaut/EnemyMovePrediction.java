2cd2090c54d16ca2ce86be34c0f111a6b1ec0106
Enrico <envomp@ttu.ee>
2019-05-10 19:05:12 +0300

    ready4pull github

--- Agonaut/EnemyMovePrediction.java
+++ Agonaut/EnemyMovePrediction.java
@@ -3,14 +3,12 @@ package Agonaut;
 import java.awt.geom.Point2D.Double;
 
 class EnemyMovePrediction {
-    Double fireLocation;
-    long fireTime;
-    double bulletVelocity;
-    double directAngle;
-    double distanceTraveled;
-    int direction;
+  Double fireLocation;
+  long fireTime;
+  double bulletVelocity;
+  double directAngle;
+  double distanceTraveled;
+  int direction;
 
-    EnemyMovePrediction() {
-    }
-
+  EnemyMovePrediction() {}
 }
--- Agonaut/SingleEnemy.java
+++ Agonaut/SingleEnemy.java
@@ -3,82 +3,81 @@ package Agonaut;
 import robocode.Robot;
 import robocode.ScannedRobotEvent;
 
-
 class SingleEnemy {
-    private String name;
+  private String name;
 
-    private double bearing;
-    private double energy;
-    private double heading;
-    private double lat;
-    private double lon;
-    private double radius;
-    private double speed;
+  private double bearing;
+  private double energy;
+  private double heading;
+  private double lat;
+  private double lon;
+  private double radius;
+  private double speed;
 
-    SingleEnemy() {
-        this.reset();
+  SingleEnemy() {
+    this.reset();
+  }
+
+  void reset() {
+    this.bearing = 0.0D;
+    this.radius = 0.0D;
+    this.energy = 0.0D;
+    this.heading = 0.0D;
+    this.speed = 0.0D;
+    this.name = "";
+    this.lat = 0.0D;
+    this.lon = 0.0D;
+  }
+
+  void update(ScannedRobotEvent e, Robot robot) {
+    double bearingDegree = robot.getHeading() + e.getBearing();
+    if (bearingDegree < 0.0D) {
+      bearingDegree += 360.0D;
     }
+    this.name = e.getName();
 
-    void reset() {
-        this.bearing = 0.0D;
-        this.radius = 0.0D;
-        this.energy = 0.0D;
-        this.heading = 0.0D;
-        this.speed = 0.0D;
-        this.name = "";
-        this.lat = 0.0D;
-        this.lon = 0.0D;
-    }
+    this.bearing = e.getBearingRadians();
+    this.energy = e.getEnergy();
+    this.heading = e.getHeadingRadians();
+    this.lat = robot.getX() + Math.sin(Math.toRadians(bearingDegree)) * e.getDistance();
+    this.lon = robot.getY() + Math.cos(Math.toRadians(bearingDegree)) * e.getDistance();
+    this.radius = e.getDistance();
+    this.speed = e.getVelocity();
+  }
 
-    void update(ScannedRobotEvent e, Robot robot) {
-        double bearingDegree = robot.getHeading() + e.getBearing();
-        if (bearingDegree < 0.0D) {
-            bearingDegree += 360.0D;
-        }
-        this.name = e.getName();
+  boolean none() {
+    return this.name.length() == 0;
+  }
 
-        this.bearing = e.getBearingRadians();
-        this.energy = e.getEnergy();
-        this.heading = e.getHeadingRadians();
-        this.lat = robot.getX() + Math.sin(Math.toRadians(bearingDegree)) * e.getDistance();
-        this.lon = robot.getY() + Math.cos(Math.toRadians(bearingDegree)) * e.getDistance();
-        this.radius = e.getDistance();
-        this.speed = e.getVelocity();
-    }
+  String getName() {
+    return name;
+  }
 
-    boolean none() {
-        return this.name.length() == 0;
-    }
+  double getBearing() {
+    return bearing;
+  }
 
-    String getName() {
-        return name;
-    }
+  double getEnergy() {
+    return energy;
+  }
 
-    double getBearing() {
-        return bearing;
-    }
+  double getHeading() {
+    return heading;
+  }
 
-    double getEnergy() {
-        return energy;
-    }
+  double getLat() {
+    return lat;
+  }
 
-    double getHeading() {
-        return heading;
-    }
+  double getLon() {
+    return lon;
+  }
 
-    double getLat() {
-        return lat;
-    }
+  double getRadius() {
+    return radius;
+  }
 
-    double getLon() {
-        return lon;
-    }
-
-    double getRadius() {
-        return radius;
-    }
-
-    double getSpeed() {
-        return speed;
-    }
+  double getSpeed() {
+    return speed;
+  }
 }
